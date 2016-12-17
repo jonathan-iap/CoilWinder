1d591d24bd5f592e92cb2baa4b6652ddf8aa711e
jonathan-iap <jonathan.iapicco@hotmail.fr>
2016-12-17 23:21:03 +0100

    v0.17-ok read eeprom and set value

--- Arduino/src/MenuSettings.h
+++ Arduino/src/MenuSettings.h
@@ -16,23 +16,34 @@
 #include "LiquidCrystal_I2C.h"
 #include "ClickEncoder.h"
 #include "Function.h"
+#include "Save.h"
 
-class Setting
+class Setting: public Memory
 {
 public :
 
   Setting(LiquidCrystal_I2C *p_lcd, ClickEncoder *p_Encoder);
   ~Setting();
 
-  float setValue(char value[], int arraySize, const char label[]);
+  void getId(const uint8_t id);
 
 private:
 
   LiquidCrystal_I2C *_lcd;
   ClickEncoder *_Encoder;
 
+  uint8_t _idValue;
+  uint8_t _buffSize;
+  double *p_floatingValue;
+  char *p_arrayValue;
+  char _label[17]; // Minimum lcd screen 16 + 1 for null character
+
+  void affectValues();
+  float engine();
   uint8_t ignoreChar(uint8_t _index, uint8_t _last, char value[], int _arraySize);
   void blinkValue(uint8_t _index, char value[], int _arraySize, bool _blank);
+  void setValue();
+
 };
 
 #endif /* SRC_MENUSETTINGS_H_ */
